a5cabe847e973717a90e3afb59e24b347b1f91d8
Aleksandra <olaszczur93@gmail.com>
2022-02-12 21:35:45 +0100

    created start component

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,13 +1,14 @@
-import Button from './atoms/Button';
-import Card from './Card';
-import ProgressBar from './atoms/ProgressBar/ProgressBar'
-import { useEffect, useState } from 'react';
-import english from './english';
+import Button from "./atoms/Button";
+import Card from "./Card";
+import ProgressBar from "./atoms/ProgressBar/ProgressBar";
+import { useEffect, useState } from "react";
+import english from "./english";
 
 function Container() {
   // const [data, setData] = useState({});
   const [toggle, setToggle] = useState(false);
-  const [index, setIndex] = useState(0);
+  const [gameStarted, setGameStarted] = useState(true);
+  const [index, setIndex] = useState(null);
   // const [indexesToOmit, setIndexesToOmit] = useState([]);
   const [arrayOfIndexes, setArrayOfIndexes] = useState([]);
 
@@ -17,16 +18,26 @@ function Container() {
   //           .then(myJson => setData(myJson));
   // };
 
-  const toggleCard  =  () => {
-    setToggle(!toggle)
-  }
+  const toggleCard = () => {
+    setToggle(!toggle);
+  };
 
   // const handleRandom = () => {
-    // console.log("go1")
-    // setIndex(getRandomIndex());
-    // console.log("go2")
-    // console.log(arrayOfIndexes)
+  // console.log("go1")
+  // setIndex(getRandomIndex());
+  // console.log("go2")
+  // console.log(arrayOfIndexes)
   // }
+
+  const startGame = () => {
+    setGameStarted(false);
+    getRandomIndex();
+  };
+
+  const resetGame = () => {
+    setData();
+    setGameStarted(true);
+  };
 
   const getRandomIndex = () => {
     // const max = english.length
@@ -35,21 +46,22 @@ function Container() {
     //   const element = array[index];
     // }
     if (arrayOfIndexes.length === 0) {
-      setData()
-      console.log("setting")
+      setData();
+      console.log("setting");
     } else {
-      console.log("not setting")
+      console.log("not setting");
     }
-    const randomNumber = Math.floor(Math.random() * arrayOfIndexes.length)
-    const randomFromAvailableIndexes = arrayOfIndexes[randomNumber]
-    console.log(randomFromAvailableIndexes)
-    console.log(arrayOfIndexes)
+    const randomNumber = Math.floor(Math.random() * arrayOfIndexes.length);
+    const randomFromAvailableIndexes = arrayOfIndexes[randomNumber];
+    // console.log(randomFromAvailableIndexes);
+    console.log(`arrayOfIndexes: ${arrayOfIndexes}`);
     // var index = array.indexOf(item);
-// if (index !== -1) {
-  // array.splice(index, 1);
-  // }
-    setArrayOfIndexes(arrayOfIndexes
-      .filter(val => val !== randomFromAvailableIndexes))
+    // if (index !== -1) {
+    // array.splice(index, 1);
+    // }
+    setArrayOfIndexes(
+      arrayOfIndexes.filter((val) => val !== randomFromAvailableIndexes)
+    );
     // const randomIndex = Math.floor(Math.random() * max + min)
     // const randomIndex = Math.floor(Math.random() * max);
     // console.log(randomIndex)
@@ -60,45 +72,60 @@ function Container() {
     // console.log(arrayOfIndexes)
 
     setIndex(randomFromAvailableIndexes);
-  }
+  };
 
   useEffect(() => {
-    setData()
+    setData();
     // console.log(arrayOfIndexes.length)
     // console.log(arrayOfIndexes[5])
     // console.log(arrayOfIndexes[6])
     // console.log("settttt")
-  }, [])
+  }, []);
 
   const setData = () => {
     let indexesWithoutCurrent = Array(english.length)
       .fill(0)
-      .map((el, index) => index)
+      .map((el, index) => index);
     setArrayOfIndexes(indexesWithoutCurrent);
-    console.log("curr " + indexesWithoutCurrent)
-    console.log("eng len " + english.length)
-  }
+    console.log("curr " + indexesWithoutCurrent);
+    console.log("eng len " + english.length);
+  };
 
   // useEffect(() => {
-    // setIndex(getRandomIndex())
-    // setIndex(1)
+  // setIndex(getRandomIndex())
+  // setIndex(1)
   // }, [])
 
   // useEffect(() => {
   //   getData();
   // }, []);
 
-    return (
+  //TODO - karta 12 się nie pokazuje (arrayOfIndexes.length + 1)
+  return (
     <>
-      <ProgressBar
-        progress={english.length - arrayOfIndexes.length}
-        maxAnswers={english.length} />
-      {arrayOfIndexes.length
-        ?
-        <Card handleClick={toggleCard}>{!toggle ? <p>{english[index].answer}</p> : <p>{english[index].question}</p>}</Card>
-        :
-        "~Fin~"}
-      <Button handleBtnClick={getRandomIndex} />
+      {gameStarted ? (
+        <>
+          <Card>Start Game</Card>
+          <Button handleBtnClick={startGame} />
+        </>
+      ) : arrayOfIndexes.length ? (
+        <>
+          <ProgressBar
+            progress={english.length - arrayOfIndexes.length}
+            maxAnswers={english.length}
+          />
+          <Card handleClick={toggleCard}>
+            {!toggle ? (
+              <p>{english[index].answer}</p>
+            ) : (
+              <p>{english[index].question}</p>
+            )}
+          </Card>
+          <Button handleBtnClick={getRandomIndex} />
+        </>
+      ) : (
+        resetGame()
+      )}
     </>
   );
 }
