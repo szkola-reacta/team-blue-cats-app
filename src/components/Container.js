4451dc511269039647bd10e53b83b7162081f61a
SzymSie <sieklucki.szymon@gmail.com>
2022-02-13 02:17:32 +0100

    pairSpontan - cleanup

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,33 +1,18 @@
-import Button from "./atoms/Button";
-import Card from "./Card";
-import ProgressBar from "./atoms/ProgressBar/ProgressBar";
-import { useEffect, useState } from "react";
-import english from "./english";
+import Button from './atoms/Button';
+import Card from './Card';
+import ProgressBar from './atoms/ProgressBar/ProgressBar';
+import { useEffect, useState } from 'react';
+import english from './english';
 
 function Container() {
-  // const [data, setData] = useState({});
   const [toggle, setToggle] = useState(false);
   const [gameStarted, setGameStarted] = useState(true);
   const [index, setIndex] = useState(null);
-  // const [indexesToOmit, setIndexesToOmit] = useState([]);
   const [arrayOfIndexes, setArrayOfIndexes] = useState([]);
-
-  // const getData = () => {
-  //   fetch("data/english.json")
-  //           .then(res => res.json())
-  //           .then(myJson => setData(myJson));
-  // };
 
   const toggleCard = () => {
     setToggle(!toggle);
   };
-
-  // const handleRandom = () => {
-  // console.log("go1")
-  // setIndex(getRandomIndex());
-  // console.log("go2")
-  // console.log(arrayOfIndexes)
-  // }
 
   const startGame = () => {
     setGameStarted(false);
@@ -40,46 +25,20 @@ function Container() {
   };
 
   const getRandomIndex = () => {
-    // const max = english.length
-    // // const randomIndex
-    // for (let index = 0; index < array.length; index++) {
-    //   const element = array[index];
-    // }
     if (arrayOfIndexes.length === 0) {
       setData();
-      console.log("setting");
-    } else {
-      console.log("not setting");
     }
     const randomNumber = Math.floor(Math.random() * arrayOfIndexes.length);
     const randomFromAvailableIndexes = arrayOfIndexes[randomNumber];
-    // console.log(randomFromAvailableIndexes);
-    console.log(`arrayOfIndexes: ${arrayOfIndexes}`);
-    // var index = array.indexOf(item);
-    // if (index !== -1) {
-    // array.splice(index, 1);
-    // }
     setArrayOfIndexes(
       arrayOfIndexes.filter((val) => val !== randomFromAvailableIndexes)
     );
-    // const randomIndex = Math.floor(Math.random() * max + min)
-    // const randomIndex = Math.floor(Math.random() * max);
-    // console.log(randomIndex)
-    // setIndex(randomIndex)
-    // return randomIndex
-    // return randomIndex
-    // return randomFromAvailableIndexes
-    // console.log(arrayOfIndexes)
 
     setIndex(randomFromAvailableIndexes);
   };
 
   useEffect(() => {
     setData();
-    // console.log(arrayOfIndexes.length)
-    // console.log(arrayOfIndexes[5])
-    // console.log(arrayOfIndexes[6])
-    // console.log("settttt")
   }, []);
 
   const setData = () => {
@@ -87,18 +46,7 @@ function Container() {
       .fill(0)
       .map((el, index) => index);
     setArrayOfIndexes(indexesWithoutCurrent);
-    console.log("curr " + indexesWithoutCurrent);
-    console.log("eng len " + english.length);
   };
-
-  // useEffect(() => {
-  // setIndex(getRandomIndex())
-  // setIndex(1)
-  // }, [])
-
-  // useEffect(() => {
-  //   getData();
-  // }, []);
 
   //TODO - karta 12 się nie pokazuje (arrayOfIndexes.length + 1)
   return (
--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,6 +1,5 @@
 function Button(props) {
     return (
-        // <button onClick={props.handleBtnClick(3,4)} style={{fontSize: 20}}>
         <button onClick={props.handleBtnClick} style={{fontSize: 20}}>
             d(^_^)b
         </button>
